6392ab8f53996dbcd32cfc0c3ab9e6e7d6cdb65e
Annisarhy <analisarahayu@gmail.com>
2019-08-07 10:46:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lat_intent/IntentActivity.java
@@ -0,0 +1,15 @@
+package com.example.lat_intent;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        //AUTO GENERATE
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
